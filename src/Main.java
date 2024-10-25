c20899d98dbade54c0baeb7255c712f302dc60d2
f0tt3r <dokyo.kun@mail.ru>
2024-10-26 00:45:16 +0200

    hier habe isLeapDay hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static boolean isLeapDay(int y) {
+        return (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+    }
 
     public static String yourDate(int d, int m, int y, int i) {
         while (i != 0) {
-            if ((m == 2 && d == 28) || (m%2 == 1 && m < 8 && d == 31) || (m%2 == 0 && m < 8 && d == 30) || (m%2 == 1 && m > 8 && d == 30) || (m%2 == 0 && m >= 8 && d == 31)) {
+            if ((m == 2 && d == (isLeapDay(y) ? 29 : 28)) || (m%2 == 1 && m < 8 && d == 31) || (m%2 == 0 && m < 8 && d == 30) || (m%2 == 1 && m > 8 && d == 30) || (m%2 == 0 && m >= 8 && d == 31)) {
                 /*
-                1. Monat: 2
+                1. Monat: 2 = 28 oder 29
                 2. Monaten: 1, 3, 5, 7 = 31
                 3. Monaten: 4, 6 = 30
                 4. Monaten: 9, 11 = 30
                 5. Monaten: 8, 10, 12 = 31
-
-                Wenn man an 1 Stelle Bedingter Ausdruck mit zusätzlicher Funktion isLeapDay() addiert, kann man Schaltjahre dazu berechnen.
                  */
                 d = 1;
                 if (m == 12) { //falls Monat 12 dann wird zu 1 statt m++
